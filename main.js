737f0e6880952e90920866b5e2834852c9ffb051
Huynh Minh Hoang <hoanghuynh0609@gmail.com>
2020-11-04 14:59:05 +0700

    update app

--- main.js
+++ main.js
@@ -1,4 +1,16 @@
 window.addEventListener("scroll", function(){
     var header = document.querySelector("nav");
+    var toTop = document.querySelector(".to-top");
     header.classList.toggle("sticky", window.scrollY > 0);
+    if(window.pageYOffset > 100) {
+        toTop.classList.add("active");
+    }
+    else{
+        toTop.classList.remove("active");
+    }
 })
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+   
