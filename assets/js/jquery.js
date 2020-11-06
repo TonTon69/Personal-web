d8734514782ddd5ee88a01bec860b1a6f6178d7f
Huynh Minh Hoang <hoanghuynh0609@gmail.com>
2020-11-06 14:29:13 +0700

    update responsive

---
+++ assets/js/jquery.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $(".menu-btn").click(function() {
+        $(".navbar-nav").toggleClass("active");
+        $(".menu-btn i").toggleClass("active");
+    })
+})
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,17 @@
 window.addEventListener("scroll", function(){
     var header = document.querySelector("nav");
     var toTop = document.querySelector(".to-top");
+    
     header.classList.toggle("sticky", window.scrollY > 0);
     if(window.pageYOffset > 100) {
-        toTop.classList.add("active");
+        toTop.classList.add("show");
     }
     else{
-        toTop.classList.remove("active");
+        toTop.classList.remove("show");
     }
 })
 function topFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
+
